e2a0f4f25b7a6103846fb423c5c5f53087bf3dea
JoseGabrielZeballos <a2019117789@estudiantes.upsa.edu.bo>
2021-04-08 19:53:06 -0400

    Se creo el controlador People con token.

---
+++ Persontoken/Persontoken/Models/DataContext.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+
+namespace Persontoken.Models
+{
+    public class DataContext:DbContext
+    {
+        public DataContext():base("DefaultConnection")
+        {
+
+        }
+
+        public System.Data.Entity.DbSet<Persontoken.Models.Person> People { get; set; }
+    }
+}
---
+++ Persontoken/Persontoken/Models/Person.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace Persontoken.Models
+{
+    public class Person
+    {
+        [Key]
+
+        public int Personid { get; set; }
+        [Required]
+        public string Name { get; set; }
+
+        public int CovidStatus { get; set; }
+    }
+}
