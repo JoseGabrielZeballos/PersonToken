be1462117e27dc95e1c2befad419915af6991f43
JoseGabrielZeballos <a2019117789@estudiantes.upsa.edu.bo>
2021-04-10 11:19:52 -0400

    Se agrego el controlador mvc de people

---
+++ Persontoken/Webpersontoken/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(Webpersontoken.Startup))]
+namespace Webpersontoken
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
