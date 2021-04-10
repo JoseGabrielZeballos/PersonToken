cd94e7efbd0ad1003ac312dc00267f1ce11a0880
JoseGabrielZeballos <a2019117789@estudiantes.upsa.edu.bo>
2021-04-10 11:30:11 -0400

    Se agrego el people index

--- Persontoken/Webpersontoken/Controllers/PeopleController.cs
+++ Persontoken/Webpersontoken/Controllers/PeopleController.cs
@@ -15,12 +15,14 @@ namespace Webpersontoken.Controllers
         private DataContext db = new DataContext();
 
         // GET: People
+        [Authorize]
         public ActionResult Index()
         {
             return View(db.People.ToList());
         }
 
         // GET: People/Details/5
+        [Authorize]
         public ActionResult Details(int? id)
         {
             if (id == null)
@@ -36,6 +38,7 @@ namespace Webpersontoken.Controllers
         }
 
         // GET: People/Create
+        [Authorize]
         public ActionResult Create()
         {
             return View();
@@ -44,6 +47,7 @@ namespace Webpersontoken.Controllers
         // POST: People/Create
         // Para protegerse de ataques de publicación excesiva, habilite las propiedades específicas a las que quiere enlazarse. Para obtener 
         // más detalles, vea https://go.microsoft.com/fwlink/?LinkId=317598.
+        [Authorize]
         [HttpPost]
         [ValidateAntiForgeryToken]
         public ActionResult Create([Bind(Include = "Personid,Name,CovidCount")] Person person)
@@ -59,6 +63,7 @@ namespace Webpersontoken.Controllers
         }
 
         // GET: People/Edit/5
+        [Authorize]
         public ActionResult Edit(int? id)
         {
             if (id == null)
@@ -76,6 +81,7 @@ namespace Webpersontoken.Controllers
         // POST: People/Edit/5
         // Para protegerse de ataques de publicación excesiva, habilite las propiedades específicas a las que quiere enlazarse. Para obtener 
         // más detalles, vea https://go.microsoft.com/fwlink/?LinkId=317598.
+        [Authorize]
         [HttpPost]
         [ValidateAntiForgeryToken]
         public ActionResult Edit([Bind(Include = "Personid,Name,CovidCount")] Person person)
@@ -90,6 +96,7 @@ namespace Webpersontoken.Controllers
         }
 
         // GET: People/Delete/5
+        [Authorize]
         public ActionResult Delete(int? id)
         {
             if (id == null)
@@ -105,6 +112,7 @@ namespace Webpersontoken.Controllers
         }
 
         // POST: People/Delete/5
+        [Authorize]
         [HttpPost, ActionName("Delete")]
         [ValidateAntiForgeryToken]
         public ActionResult DeleteConfirmed(int id)
